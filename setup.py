0a70a6758820cb68d471bfd588550fb7a0368a44
Vitor <vmenezes599@gmail.com>
2025-05-11 09:55:55 +0200

    Moving folders.

--- setup.py
+++ setup.py
@@ -6,11 +6,12 @@ from setuptools import setup, find_packages
 
 setup(
     name="ai_video_creator",
-    version="0.2.1",
+    version="0.2.2",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
-            "comfyui_manager=lib.ComfyUI_automation.comfyui_manager:main",
+            "comfyui_manager=ai_video_creator.ComfyUI_automation.comfyui_manager:main",
+            "ai_video_creator=ai_video_creator.ai_video_creator:main",
         ],
     },
 )
