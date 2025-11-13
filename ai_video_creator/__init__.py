ec358fe2275627e67837af2c2b691d6da16161ce
Vitor <vmenezes599@gmail.com>
2025-11-13 20:47:14 +0100

    Separated image and narrator generation.

--- ai_video_creator/__init__.py
+++ ai_video_creator/__init__.py
@@ -3,26 +3,30 @@ AI Video Creator Package
 """
 
 from .video_creator import (
-    create_narrator_and_image_assets,
-    create_narrator_and_image_recipe,
     create_background_music_recipe,
     create_background_music_assets,
     create_assemble_video_recipe,
     create_sub_video_recipes,
     create_sub_videos_assets,
+    create_narrator_assets,
+    create_narrator_recipe,
+    create_images_assets,
+    create_image_recipe,
     clean_unused_assets,
     assemble_video,
 )
 from .utils import AspectRatios
 
 __all__ = [
-    "create_narrator_and_image_assets",
-    "create_narrator_and_image_recipe",
     "create_background_music_recipe",
     "create_background_music_assets",
     "create_assemble_video_recipe",
     "create_sub_video_recipes",
     "create_sub_videos_assets",
+    "create_narrator_recipe",
+    "create_narrator_assets",
+    "create_images_assets",
+    "create_image_recipe",
     "clean_unused_assets",
     "assemble_video",
     "AspectRatios",
